6f4a221906387a23f5e129b106f3a740299f1c2e
Tzu-Sheng Kuo <awinder0230@gmail.com>
2023-05-23 20:45:53 -0400

    entity page backup

--- js/entity.js
+++ js/entity.js
@@ -12,64 +12,77 @@
             editDamage: "edit damage",
             userIntent: "user intent"
         };
+        var facetLabels = {
+            editDamage: ["damaging", "not damaging"],
+            userIntent: ["bad faith", "good faith"]
+        };
 
+        // get config
         var wgPageName = mw.config.get("wgPageName");
 
         if(wgPageName.startsWith(entityPagePrefix) && mw.config.get("wgAction") === "view") {
-            var mwApi = new mw.Api();
-            
+
+            // widgets
             var diffTableHeader = "<table class=\"diff diff-contentalign-left diff-editfont-monospace\" data-mw=\"interface\"><colgroup><col class=\"diff-marker\"><col class=\"diff-content\"><col class=\"diff-marker\"><col class=\"diff-content\"></colgroup><tbody>";
             var diffTableFooter = "</tbody></table>";
             var diffTableContent, diffTableTitle;
+            var noticeBox;
+            var primaryFieldset;
+            var userFieldset;
+            var individualFieldset = {};
+            var stackBars = {};
+            var stackBarText = {};
+
+            var mwApi = new mw.Api();
+            
 
             var entityId = Number(wgPageName.substring(entityPagePrefix.length));
             var userName = mw.config.get("wgUserName");
             var userId = mw.config.get("wgUserId");
             var revisionId = mw.config.get("wgRevisionId");
 
-            mw.loader.using(["oojs-ui-core", "oojs-ui-widgets", "oojs-ui-windows", "mediawiki.diff.styles"]).done(function () {
-                mwApi.get({
-                    action: "compare",
-                    fromrev: entityId,
-                    torelative: "prev",
-                }).done(function(ret) {
-                    diffTableContent = ret.compare["*"];
-                    diffTableTitle = 
-                    `<tr class="diff-title" lang="${language}">
-                        <td colspan="2" class="diff-otitle diff-side-deleted">
-                            <div id="mw-diff-otitle1">
-                                <strong><a href="/w/index.php?oldid=${ret.compare.fromrevid}" title="${ret.compare.fromtitle}">Previous revision</a>
-                            </div>
-                            <div id="mw-diff-otitle5"></div>
-                        </td>
-                        <td colspan="2" class="diff-ntitle diff-side-added">
-                            <div id="mw-diff-ntitle1">
-                                <strong><a href="/w/index.php?oldid=${ret.compare.torevid}" title="${ret.compare.fromtitle}">Edited revision</a>
-                            </div>
-                            <div id="mw-diff-ntitle5"></div>
-                         </td>
-                    </tr>`
-                });
-            }).done(function(){
+            var getDiffContent = mwApi.get({
+                action: "compare",
+                fromrev: entityId,
+                torelative: "prev",
+            });
 
-                mwApi.get({
-                    action: "parse",
-                    page: wgPageName,
-                    prop: "wikitext"
-                }).done(function(ret) {
-                    $(".mw-parser-output").find("table").remove(); // remove warning message
-                    $(".mw-parser-output").find("hr").remove(); // remove horizontal line
-                    $(".mw-parser-output").find("p").remove(); // remove json content
+            var setDiffTable = getDiffContent.then(function(ret) {
+                diffTableContent = ret.compare["*"];
+                diffTableTitle = 
+                `<tr class="diff-title" lang="${language}">
+                    <td colspan="2" class="diff-otitle diff-side-deleted">
+                        <div id="mw-diff-otitle1">
+                            <strong><a href="/w/index.php?oldid=${ret.compare.fromrevid}" title="${ret.compare.fromtitle}">Previous revision</a>
+                        </div>
+                        <div id="mw-diff-otitle5"></div>
+                    </td>
+                    <td colspan="2" class="diff-ntitle diff-side-added">
+                        <div id="mw-diff-ntitle1">
+                            <strong><a href="/w/index.php?oldid=${ret.compare.torevid}" title="${ret.compare.fromtitle}">Edited revision</a>
+                        </div>
+                        <div id="mw-diff-ntitle5"></div>
+                        </td>
+                </tr>`;
+            });
+
+            var getPageContent = mwApi.get({
+                action: "parse",
+                page: wgPageName,
+                prop: "wikitext"
+            });
+
+            var setPageWidgets = getPageContent.then(function(ret) {
+                mw.loader.using(["oojs-ui-core", "oojs-ui-widgets", "oojs-ui-windows", "mediawiki.diff.styles"]).done(function(){
                     var label = JSON.parse(ret.parse.wikitext["*"].split(entityPageSplit)[1]);
 
-                    var noticeBox = new OO.ui.MessageWidget({
+                    noticeBox = new OO.ui.MessageWidget({
                         type: "notice",
                         label: "Please do not directly edit source of this page. To update the primary or your label, click the edit buttons. To discuss, visit the talk page."
                     });
 
                     // Primary label
-
-                    var primaryFieldset = new OO.ui.FieldsetLayout({ 
+                    primaryFieldset = new OO.ui.FieldsetLayout({ 
                         label: "Primary label",
                         classes: ["wikibench-entity-primary-label"],
                     });
@@ -109,7 +122,6 @@
 
                     var editPrimaryBtn = new OO.ui.ButtonWidget({
                         label: "Edit"
-                        // flags: ["primary", "progressive"]
                     });
 
                     primaryFieldset.addItems(
@@ -119,8 +131,7 @@
                     );
 
                     // User label
-
-                    var userFieldset = new OO.ui.FieldsetLayout({ 
+                    userFieldset = new OO.ui.FieldsetLayout({ 
                         label: "Your label (" + userName + ")",
                         classes: ["wikibench-entity-user-label"]
                     });
@@ -157,114 +168,97 @@
                         })
                     ]);
 
+                    var divRender = $(".mw-parser-output");
+
+                    divRender.find("table").remove(); // remove warning message
+                    divRender.find("hr").remove(); // remove horizontal line
+                    divRender.find("p").remove(); // remove json content
+
+                    divRender
+                        .append(noticeBox.$element)
+                        .append(diffTableHeader + diffTableTitle + diffTableContent + diffTableFooter)
+                        .append(primaryFieldset.$element)
+                        .append(userFieldset.$element);
+
                     // individual labels
-                    
-                    var fieldset1 = new OO.ui.FieldsetLayout( {
-                        icon: "expand",
-                        label: 'Damaging (2)',
-                        classes: ['wikibench-entity-individual-labels']
-                    } );
-                    
-                    fieldset1.addItems([
-                        new OO.ui.FieldLayout(
-                            new OO.ui.LabelWidget({label: 'Comment'}),
-                            {label: "user1"}
-                        ),
-                        new OO.ui.FieldLayout(
-                            new OO.ui.LabelWidget({label: 'Comment'}),
-                            {label: "user2"}
-                        )
-                    ]);
-
-                    var fieldset2 = new OO.ui.FieldsetLayout( {
-                        icon: "expand",
-                        label: 'Not Damaging (2)',
-                        classes: ['wikibench-entity-individual-labels']
-                    } );
-                    
-                    fieldset2.addItems([
-                        new OO.ui.FieldLayout(
-                            new OO.ui.LabelWidget({label: 'Comment'}),
-                            {label: "user1"}
-                        ),
-                        new OO.ui.FieldLayout(
-                            new OO.ui.LabelWidget({label: 'Comment'}),
-                            {label: "user2"}
-                        )
-                    ]);
-
-                    fieldset1.$group.toggle(false);
-                    fieldset2.$group.toggle(false);
-
-                    mwApi.get({
-                        action: "parse",
-                        text: "{{Stacked bar|height=18px|A1=30|C1=#b32424|T1=damaging |A2=20|C2=#fee7e6|T2=likely damaging |A3=20|C3=#d5fdf4|T3=likely not damaging |A4=30|C4=#14866d|T4=not damaging |Total=100}}",
-                        contentmodel: "wikitext"
-                    }).done(function(ret){
-                        $(".mw-parser-output")
-                            .append(noticeBox.$element)
-                            .append(diffTableHeader + diffTableTitle + diffTableContent + diffTableFooter)
-                            // .append(tableheader + teststring + tableend)
-                            .append(primaryFieldset.$element)
-                            .append(userFieldset.$element)
-                            .append("<h2>Edit damage</h2>")
-                            .append("<h3>Label distribution</h3>")
-                            .append(ret.parse.text["*"])
-                            .append("<h3>Individual labels</h3>")
-                            .append(fieldset1.$element)
-                            .append(fieldset2.$element)
-                            .append("<h2>User intent</h2>")
-                            .append("<h3>Label distribution</h3>")
-                            .append(ret.parse.text["*"])
-                            .append("<h3>Individual labels</h3>");
-                        
-                        $(".wikibench-entity-primary-label").css({
-                            "background-color": "#eaf3ff",
-                            "padding": "10px",
-                            "margin": "11px 0px"
-                        });
-                        
-                        $(".wikibench-entity-user-label").css({
-                            "background-color": "#f8f9fa",
-                            "padding": "10px",
-                            "margin": "11px 0px"
-                        });
-                        
-                        $(".wikibench-entity-individual-labels > .oo-ui-fieldsetLayout-header").click(function(){
-                            var header = $(this);
-                            var content = header.next();
-                            //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
-                            content.slideToggle(function () {
-                                //execute this after slideToggle is done
-                                //change text of header based on visibility of content div
-                                // header.children(".oo-ui-labelElement-label").text(function () {
-                                //     return content.is(":visible") ? "Collapse" : "Expand";
-                                // });
-                                header.children(".oo-ui-iconElement-icon").toggleClass("oo-ui-icon-collapse").toggleClass("oo-ui-icon-expand");
+                    facets.forEach(function(f) {
+                        individualFieldset[f] = {};
+                        var labelCount = {};
+                        var labelLowConfidenceCount = {};
+                        facetLabels[f].forEach(function(l) {
+                            labelCount[l] = 0;
+                            labelLowConfidenceCount[l] = 0;
+                            individualFieldset[f][l] = new OO.ui.FieldsetLayout({
+                                icon: "expand",
+                                classes: ["wikibench-entity-individual-labels"]
                             });
-                        })
-
-                        $(".wikibench-entity-individual-labels").css({
-                            "background-color": "#f8f9fa",
-                            "padding": "10px",
-                            "margin": "11px 0px"
-                            //"width": "40%",
-                            //"display": "inline-block"
+                            label.facets[f].individualLabels.forEach(function(individualLabel) {
+                                if (individualLabel.label === l) {
+                                    individualFieldset[f][l].addItems(
+                                        new OO.ui.FieldLayout(
+                                            new OO.ui.LabelWidget({label: individualLabel.note}),
+                                            {label: $("<a>")
+                                                .attr("href","/wiki/User:"+individualLabel.userName)
+                                                .text(individualLabel.userName)
+                                            }
+                                        )
+                                    );
+                                    labelCount[l]++;
+                                    if (individualLabel.lowConfidence) labelLowConfidenceCount[l]++;
+                                }
+                            });
+                            individualFieldset[f][l].setLabel(l + " (" + labelCount[l].toString() + ")");
+                            individualFieldset[f][l].$group.toggle(false);
                         });
 
-                        $(".wikibench-entity-individual-labels > .oo-ui-fieldsetLayout-header").css({
-                            "cursor": "pointer"
+                        // stack bars (assume binary labels)
+                        var stackBarCounts = [
+                            labelCount[facetLabels[f][0]] - labelLowConfidenceCount[facetLabels[f][0]],
+                            labelLowConfidenceCount[facetLabels[f][0]],
+                            labelLowConfidenceCount[facetLabels[f][1]],
+                            labelCount[facetLabels[f][1]] - labelLowConfidenceCount[facetLabels[f][1]]
+                        ];
+                        var stackBarNames = [
+                            facetLabels[f][0],
+                            "likely " + facetLabels[f][0],
+                            "likely " + facetLabels[f][1],
+                            facetLabels[f][1]
+                        ];
+                        var stackBarColors = ["#b32424", "#fee7e6", "#d5fdf4", "#14866d"]
+                        stackBarText[f] = "{{Stacked bar|height=18px|";
+                        for (var i = 0; i < 4; i++) {
+                            if (stackBarCounts[i] > 0) {
+                                var tmp = (i+1).toString();
+                                stackBarText[f] += "A" + tmp + "=" + stackBarCounts[i].toString() + "|C" + tmp + "=" + stackBarColors[i] + "|T" + tmp + "=" + stackBarNames[i] + "|";
+                            }
+                        }
+                        stackBarText[f] += "Total=" + (stackBarCounts.reduce((a, b) => a + b, 0)).toString() + "}}";
+
+                        mwApi.get({
+                            action: "parse",
+                            text: stackBarText[f],
+                            contentmodel: "wikitext"
+                        }).done(function(ret) {
+                            stackBars[f] = ret.parse.text["*"];
+                            divRender
+                                .append("<h2>" + facetNames[f].charAt(0).toUpperCase() + facetNames[f].slice(1) + "</h2>")
+                                .append("<h3>Label distribution</h3>")
+                                .append(stackBars[f])
+                                .append("<h3>Individual labels</h3>");
+                            facetLabels[f].forEach(function(l) {
+                                divRender.append(individualFieldset[f][l].$element);
+                                console.log("add element");
+                                individualFieldset[f][l].$element.find(".oo-ui-fieldsetLayout-header").click(function(){
+                                    var header = $(this);
+                                    var content = header.next();
+                                    content.slideToggle(function () {
+                                        header.children(".oo-ui-iconElement-icon").toggleClass("oo-ui-icon-collapse").toggleClass("oo-ui-icon-expand");
+                                    });
+                                })
+                            });
                         });
-
-                        $(".wikibench-entity-individual-labels").find(".oo-ui-iconElement-icon").css({
-                            "margin": "10px 0 0 0"
-                        });
-
-                        // $("head").append('<link rel="stylesheet" href="/w/load.php?lang=en&modules=ext.RevisionSlider.lazyCss%2Cnoscript%7Cext.cite.styles%7Cext.uls.interlanguage%7Cext.visualEditor.desktopArticleTarget.noscript%7Cext.visualEditor.diffPage.init.styles%7Cext.wikimediaBadges%7Cjquery.makeCollapsible.styles%7Cmediawiki.diff.styles%7Cmediawiki.helplink%7Cmediawiki.interface.helpers.styles%7Cmediawiki.ui.button%2Cicon%7Cmediawiki.widgets.styles%7Coojs-ui-core.icons%2Cstyles%7Coojs-ui.styles.icons-accessibility%2Cicons-editing-advanced%2Cindicators%7Cskins.vector.icons%2Cstyles%7Cwikibase.client.init&only=styles&skin=vector-2022">');
-
                     });
-
-                })
+                });
             });
         }
     });
