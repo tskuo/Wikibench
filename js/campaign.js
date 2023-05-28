94f2c78b378ef22ff9070d792ccd41e124bad9dd
Tzu-Sheng Kuo <awinder0230@gmail.com>
2023-05-27 23:51:03 -0400

    show individual labels

--- js/campaign.js
+++ js/campaign.js
@@ -3,19 +3,25 @@
         var wgPageName = "User:Tzusheng/sandbox/Wikipedia:Wikibench/Campaign:Editquality";
         if (mw.config.get("wgPageName") === wgPageName && mw.config.get("wgAction") === "view") {
 
+            // init
+            var wikibenchURL = "User:Tzusheng/sandbox/Wikipedia:Wikibench/";
+            var entityType = "diff";
+            var language = "en";
+            var entityPageSplit = "-----";
+            var facets = ["editDamage", "userIntent"];
+            var userName = mw.config.get("wgUserName");
+
             var mwApi = new mw.Api();
-
             var allLabels = [];
-
             var prefixsearchParams = {
                 action: "query",
                 list: "prefixsearch",
-                pssearch: "User:Tzusheng/sandbox/Wikipedia:Wikibench/Diff:",
+                pssearch: wikibenchURL + entityType.charAt(0).toUpperCase() + entityType.slice(1) + ":",
                 format: "json"
             }
 
             mwApi.get(prefixsearchParams).done(function(prefixsearchData) {
-                var pages = prefixsearchData.query.prefixsearch; // array for pages
+                var pages = prefixsearchData.query.prefixsearch; // pages is an array
                 var pageTitles = [];
                 for (var p = 0; p < pages.length; p++) {
                     pageTitles.push(pages[p].title);
@@ -30,40 +36,71 @@
                 }
                 
                 mwApi.get(revisionParams).done(function(revisionData) {
-                    var revisions = revisionData.query.pages;
+                    var revisions = revisionData.query.pages; // revisions is an object, not array
                     var label;
-                    var table = $(".wikibench-data-table").find("tbody");
+                    // var tableHTML = "<table class=\"wikitable sortable jquery-tablesorter\" id=\"wikibench-table-id\">" + 
+                    //     "<thead>" + 
+                    //         "<tr>" +
+                    //             "<th rowspan=\"2\" class=\"headerSort\" tabindex=\"0\" role=\"columnheader button\" title=\"Sort ascending\">Diff ID</th>" +
+                    //             "<th colspan=\"3\">Edit damage</th>" +
+                    //             "<th colspan=\"3\">User intent</th>" +
+                    //             "<th rowspan=\"2\" class=\"headerSort\" tabindex=\"0\" role=\"columnheader button\" title=\"Sort ascending\">Label count</th>" +
+                    //             "</tr>" +
+                    //         "<tr>" +
+                    //             "<th class=\"headerSort\" tabindex=\"0\" role=\"columnheader button\" title=\"Sort ascending\">Primary label</th>" +
+                    //             "<th class=\"headerSort\" tabindex=\"0\" role=\"columnheader button\" title=\"Sort ascending\">Your label</th>" +
+                    //             "<th class=\"headerSort\" tabindex=\"0\" role=\"columnheader button\" title=\"Sort ascending\">Disagreement</th>" +
+                    //             "<th class=\"headerSort\" tabindex=\"0\" role=\"columnheader button\" title=\"Sort ascending\">Primary label</th>" +
+                    //             "<th class=\"headerSort\" tabindex=\"0\" role=\"columnheader button\" title=\"Sort ascending\">Your label</th>" +
+                    //             "<th class=\"headerSort\" tabindex=\"0\" role=\"columnheader button\" title=\"Sort ascending\">Disagreement</th>" +
+                    //         "</tr>" +
+                    //     "</thead>" +
+                    //     "<tbody>" +
+                    //         "<tr>" +
+                    //             "<th scope=\"row\"> </th>" +
+                    //             "<td> </td>" +
+                    //             "<td> </td>" +
+                    //             "<td> </td>" +
+                    //             "<td> </td>" +
+                    //             "<td> </td>" +
+                    //             "<td> </td>" +
+                    //             "<td> </td>" +
+                    //         "</tr>" +
+                    //     "</tbody>" + 
+                    //     "<tfoot></tfoot>" +
+                    // "</table>";
+                    // $("#enwiki-editquality").after(tableHTML);
+                    // var table = $("#wikibench-table-id").find("tbody");
+                    var table = $(".ttt").find("tbody");
                     table.find("tr").remove();
                     for (var r in revisions) {
-                        label = JSON.parse(revisions[r].revisions[0]["*"].split("-----")[1]);
+                        label = JSON.parse(revisions[r].revisions[0]["*"].split(entityPageSplit)[1]);
 
-                        var editDamagePrimary = label.facets.editDamage.primaryLabel.label;
-                        var userIntentPrimary = label.facets.userIntent.primaryLabel.label;
+                        var primaryLabel = {};
+                        var userLabels = {};
 
-                        var editDamageLabels = [];
-                        var userIntentLabels = [];
-
-                        for (var e = 0; e < label.facets.editDamage.individualLabels.length; e++) {
-                            editDamageLabels.push(label.facets.editDamage.individualLabels[e].label);
+                        for (var i = 0; i < facets.length; i++) {
+                            var f = facets[i];
+                            primaryLabel[f] = label.facets[f].primaryLabel.label;
+                            userLabels[f] = "";
+                            for (var j = 0; j < label.facets[f].individualLabels.length; j++) {
+                                if (label.facets[f].individualLabels[j].userName === userName) {
+                                    userLabels[f] = label.facets[f].individualLabels[j].label;
+                                }
+                            }
                         }
 
-                        for (var u = 0; u < label.facets.userIntent.individualLabels.length; u++) {
-                            userIntentLabels.push(label.facets.userIntent.individualLabels[u].label);
-                        }
-
-                        if (editDamageLabels.length !== userIntentLabels.length) {
-                            console.log("ALERT: label counts differ across facets");
-                        }
-
-                        var hrefLink = "<a href=\"/wiki/User:Tzusheng/sandbox/Wikipedia:Wikibench/Diff:" + label.entityId.toString() + "\"></a>";
+                        var hrefLink = "<a href=\"/wiki/" + wikibenchURL + entityType.charAt(0).toUpperCase() + entityType.slice(1) + ":" + label.entityId.toString() + "\"></a>";
 
                         table.append($('<tr>')
                             .append($("<th>").text(label.entityId).wrapInner(hrefLink).attr("scope","row"))
-                            .append($('<td>').text(editDamagePrimary))
+                            .append($('<td>').text(primaryLabel[facets[0]]))
+                            .append($('<td>').text(userLabels[facets[0]]))
                             .append($('<td>').text(""))
-                            .append($('<td>').text(userIntentPrimary))
+                            .append($('<td>').text(primaryLabel[facets[1]]))
+                            .append($('<td>').text(userLabels[facets[1]]))
                             .append($('<td>').text(""))
-                            .append($('<td>').text(editDamageLabels.length.toString()))
+                            .append($('<td>').text(label.facets[facets[0]].individualLabels.length.toString()))
                         );
 
                         allLabels.push(label);
