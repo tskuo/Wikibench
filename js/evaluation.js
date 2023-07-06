5e22c18707f3e6f926560a9b78633ee3fc5a65f1
Tzu-Sheng Kuo <awinder0230@gmail.com>
2023-07-06 16:48:32 -0400

    evaluation style and prob

--- js/evaluation.js
+++ js/evaluation.js
@@ -7,7 +7,8 @@
                 // init
                 var WIKIBENCH_PREFIX = "Tzusheng/sandbox/Wikipedia:Wikibench";
                 var WIKIBENCH_NAMESPACE = 2;
-                var RATE_LIMIT = 3;
+                var RATE_LIMIT = 5;
+                var ROUND_PRECISION = 2;
                 var entityType = "diff";
                 var language = "en";
                 var entityPageSplit = "-----";
@@ -45,6 +46,7 @@
                 }
                 tableLabelColors["reverted"] = "#fee7e6";
                 tableLabelColors["not reverted"] = "#d5fdf4";
+                var probColors = "#72777d";
 
                 var tableRows = {};
                 var revids = [];
@@ -52,6 +54,30 @@
                 var liftwing = {};
                 var ores = {};
                 var renderCount = 0;
+
+                function calculateStandardDeviation(numbers) {
+                    // Step 1: Calculate the mean
+                    var sum = 0;
+                    for (var i = 0; i < numbers.length; i++) {
+                    sum += numbers[i];
+                    }
+                    var mean = sum / numbers.length;
+                
+                    // Step 2: Calculate the sum of squared differences
+                    var squaredDifferencesSum = 0;
+                    for (var j = 0; j < numbers.length; j++) {
+                    var difference = numbers[j] - mean;
+                    squaredDifferencesSum += difference * difference;
+                    }
+                
+                    // Step 3: Calculate the variance
+                    var variance = squaredDifferencesSum / numbers.length;
+                
+                    // Step 4: Calculate the standard deviation (square root of variance)
+                    var standardDeviation = Math.sqrt(variance);
+                
+                    return standardDeviation;
+                }
 
                 function getPrefixedPages(entityType, queryContinue, deferred, results) {
                     deferred = deferred || $.Deferred();
@@ -125,7 +151,7 @@
                             tableRows[newId]["wikibench"] = {}
                             tableRows[newId]["ores"] = {};
                             facets.forEach(function(f) {
-                                tableRows[newId]["wikibench"][f] = label.facets[f].primaryLabel.label;
+                                tableRows[newId]["wikibench"][f] = label.facets[f];
                             })
                             revids.push(newId);
                             var request1 = $.ajax({
@@ -162,23 +188,51 @@
 
                     $.when.apply(null, promises).done(function() {
                         tbody.find("tr").remove(); // remove the empty line
-                        console.log(liftwing);
                         for (var r = 0; r < revids.length; r++) {
                             var row = tableRows[revids[r]];
                             var liftwing_prediction = liftwing[revids[r]]["prediction"] ? "reverted" : "not reverted";
                             var ores_prediction_editDamage = ores[revids[r]]["damaging"]["score"]["prediction"] ? "damaging" : "not damaging";
                             var ores_prediction_userIntent = ores[revids[r]]["goodfaith"]["score"]["prediction"] ? "good faith" : "bad faith";
+                            var individualLabels = {};
+                            // calculate wikibench agreement
+                            for (var i = 0; i < facets.length; i++) {
+                                var f = facets[i];
+                                individualLabels[f] = [];
+                                for (var j = 0; j < row["wikibench"][f].individualLabels.length; j++) {
+                                    // handle individual labels for disagreements
+                                    var individualLabel = row["wikibench"][f].individualLabels[j];
+                                    if (individualLabel.label === facetLabels[f][0]) {
+                                        if (individualLabel.lowConfidence) {
+                                            individualLabels[f].push(-0.5);
+                                        }
+                                        else {
+                                            individualLabels[f].push(-1);
+                                        }
+                                    }
+                                    if (individualLabel.label === facetLabels[f][1]) {
+                                        if (individualLabel.lowConfidence) {
+                                            individualLabels[f].push(0.5);
+                                        }
+                                        else {
+                                            individualLabels[f].push(1);
+                                        }
+                                    }
+                                }
+                            }                            
+
                             var hrefLink = "<a href=\"/wiki/User:" + WIKIBENCH_PREFIX + "/Entity:" + entityType.charAt(0).toUpperCase() + entityType.slice(1) + "/" + row["entityId"] + "\"></a>";
                             tbody.append($("<tr>")
                                 .append($("<th>").text(row["entityId"]).wrapInner(hrefLink).attr("scope", "row"))
-                                .append($("<td>").text(row["wikibench"][facets[0]]).attr("bgcolor", tableLabelColors[facets[0]][row["wikibench"][facets[0]]]))
-                                .append($("<td>").text(ores_prediction_editDamage).attr("bgcolor", tableLabelColors[facets[0]][ores_prediction_editDamage]))
-                                .append($("<td>").text(ores[revids[r]]["damaging"]["score"]["probability"][ores[revids[r]]["damaging"]["score"]["prediction"].toString()].toFixed(3)))
-                                .append($("<td>").text(row["wikibench"][facets[1]]).attr("bgcolor", tableLabelColors[facets[1]][row["wikibench"][facets[1]]]))
-                                .append($("<td>").text(ores_prediction_userIntent).attr("bgcolor", tableLabelColors[facets[1]][ores_prediction_userIntent]))
-                                .append($("<td>").text(ores[revids[r]]["goodfaith"]["score"]["probability"][ores[revids[r]]["goodfaith"]["score"]["prediction"].toString()].toFixed(3)))
-                                .append($("<td>").text(liftwing_prediction).attr("bgcolor", tableLabelColors[liftwing_prediction]))
-                                .append($("<td>").text(liftwing[revids[r]]["probabilities"][liftwing[revids[r]]["prediction"].toString()].toFixed(3)))
+                                .append($("<td>").html(row["wikibench"][facets[0]].primaryLabel.label + " <span style=\"color:" + probColors + "\">(" + (1 - calculateStandardDeviation(individualLabels[facets[0]])).toFixed(ROUND_PRECISION).toString() + ")</span>").attr("bgcolor", tableLabelColors[facets[0]][row["wikibench"][facets[0]].primaryLabel.label]))
+                                // .append($("<td>").text((1 - calculateStandardDeviation(individualLabels[facets[0]])).toFixed(ROUND_PRECISION)))
+                                .append($("<td>").html(ores_prediction_editDamage + " <span style=\"color:" + probColors + "\">(" + ores[revids[r]]["damaging"]["score"]["probability"][ores[revids[r]]["damaging"]["score"]["prediction"].toString()].toFixed(ROUND_PRECISION).toString() + ")</span>").attr("bgcolor", tableLabelColors[facets[0]][ores_prediction_editDamage]))
+                                // .append($("<td>").text(ores[revids[r]]["damaging"]["score"]["probability"][ores[revids[r]]["damaging"]["score"]["prediction"].toString()].toFixed(ROUND_PRECISION)))
+                                .append($("<td>").html(row["wikibench"][facets[1]].primaryLabel.label + " <span style=\"color:" + probColors + "\">(" + (1 - calculateStandardDeviation(individualLabels[facets[1]])).toFixed(ROUND_PRECISION).toString() + ")</span>").attr("bgcolor", tableLabelColors[facets[1]][row["wikibench"][facets[1]].primaryLabel.label]))
+                                // .append($("<td>").text((1 - calculateStandardDeviation(individualLabels[facets[1]])).toFixed(ROUND_PRECISION)))
+                                .append($("<td>").html(ores_prediction_userIntent + " <span style=\"color:" + probColors + "\">(" + ores[revids[r]]["goodfaith"]["score"]["probability"][ores[revids[r]]["goodfaith"]["score"]["prediction"].toString()].toFixed(ROUND_PRECISION).toString() + ")</span>").attr("bgcolor", tableLabelColors[facets[1]][ores_prediction_userIntent]))
+                                // .append($("<td>").text(ores[revids[r]]["goodfaith"]["score"]["probability"][ores[revids[r]]["goodfaith"]["score"]["prediction"].toString()].toFixed(ROUND_PRECISION)))
+                                .append($("<td>").html(liftwing_prediction + " <span style=\"color:" + probColors + "\">(" + liftwing[revids[r]]["probabilities"][liftwing[revids[r]]["prediction"].toString()].toFixed(ROUND_PRECISION).toString() + ")</span>").attr("bgcolor", tableLabelColors[liftwing_prediction]))
+                                // .append($("<td>").text(liftwing[revids[r]]["probabilities"][liftwing[revids[r]]["prediction"].toString()].toFixed(ROUND_PRECISION)))
                             );
                         }
                     }).fail(function(e) {
